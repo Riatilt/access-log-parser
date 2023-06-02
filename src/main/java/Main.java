2dc431609ab79c76bd3bf2a1dcd505fb73ca93b9
Mary Milton <rihereri@gmail.com>
2023-06-02 19:45:42 +0300

    Существование файла, к которому указан путь

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,24 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число:");
-        int firstNumber = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число:");
-        int secondNumber = new Scanner(System.in).nextInt();
+        int count = 0;
+        while (true){
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
 
-        int sum = firstNumber + secondNumber;
-        int multi = firstNumber * secondNumber;
-        int sub = firstNumber - secondNumber;
-        double quotient = (double) firstNumber/secondNumber;
-        System.out.println("Cумма чисел = " + sum);
-        System.out.println("Произведение чисел = " + multi);
-        System.out.println("Разность чисел = " + sub);
-        System.out.println("Частное чисел = " + quotient);
+            if (!fileExists || isDirectory) {
+                System.out.println("Указанный файл не существует или не является файлом.");
+                continue;
+            } else {
+                System.out.println("Путь указан верно.");}
+            count++;
+            System.out.println("Это файл номер " + count + ".");
+        }
+
     }
+
 }
